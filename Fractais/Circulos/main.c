56c1cdd33b47d49311cdccf578ab41c7ee50b72a
eucagoempe <40449931+eucagoempe@users.noreply.github.com>
2019-03-27 20:33:23 -0300

    Circulos Funcionando

--- Fractais/Circulos/main.c
+++ Fractais/Circulos/main.c
@@ -7,12 +7,17 @@ void tree(int x, int y, float ang, float tamanho){
     xs_color((XColor) {xm_rand(0,255),xm_rand(0,255),xm_rand(0,255)});
     xd_circle(x, y, tamanho);
 
-    for(int i = 0; i < 6; i++) {
-        int x1 = x + tamanho * xm_cos(ang*i);
-        int y1 = y - tamanho * xm_sin(ang*i);
-        tree(x1, y1, ang + i*360/6, tamanho / 3);
-    }
 
+    for(int i = 0; i < 6; i++) {
+        int x1 = x + tamanho * xm_cos(ang+i*60);
+        int y1 = y - tamanho * xm_sin(ang+i*60);
+        tree(x1, y1, ang , tamanho / 3);
+        tree(x1, y1, ang + 60, tamanho / 3);
+        tree(x1, y1, ang + 2*60, tamanho / 3);
+        tree(x1, y1, ang + 3*60, tamanho / 3);
+        tree(x1, y1, ang + 4*60, tamanho / 3);
+        tree(x1, y1, ang + 5*60, tamanho / 3);
+    }
 
 
 }
--- Fractais/Quadrados/cmake-build-debug/cmake_install.cmake
+++ Fractais/Quadrados/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/untitled
+# Install script for directory: D:/ED2019/Fractais/Quadrados
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/ED2019/Fractais/Quadrados/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
